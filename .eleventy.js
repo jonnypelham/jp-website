9ad890caaf1ab1d8e47521efbf0678f6e65a536b
Jonathan Pelham <jonnypelham@gmail.com>
2023-02-26 13:49:42 +0000

    basic structure

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = (config) => {
+  config.addPassthroughCopy("./src/img/");
+
+  return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
---
+++ src/index.md
@@ -0,0 +1,7 @@
+---
+title: "Hello, world"
+layout: "layouts/home.html"
+image: "/img/ok-mr-field.jpg"
+---
+
+Hello _bean_!
